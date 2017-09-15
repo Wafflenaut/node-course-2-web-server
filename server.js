3b097aaa1d5df6e4482abe2301a3b5a67f385414
Wafflenaut <moyerjo@oregonstate.edu>
2017-09-14 21:11:34 -0400

    Added portfolio page and added package-lock.json to git ignore

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Portfolio'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({error: 'Bad Request'});
 });
